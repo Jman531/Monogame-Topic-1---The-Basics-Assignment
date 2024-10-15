2a256c4bba8b1d254594b353b2b9bcdd51731c56
Jack Gallagher <339690067@GoTVDSB.ca>
2024-10-15 09:38:58 -0400

    Class ended

--- Monogame Topic 1 - The Basics Assignment/Game1.cs
+++ Monogame Topic 1 - The Basics Assignment/Game1.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Runtime.CompilerServices;
 
 namespace Monogame_Topic_1___The_Basics_Assignment
 {
@@ -8,6 +10,8 @@ namespace Monogame_Topic_1___The_Basics_Assignment
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        Texture2D fieldTexture, birdTexture, deerTextrure;
 
         public Game1()
         {
@@ -19,6 +23,11 @@ namespace Monogame_Topic_1___The_Basics_Assignment
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            _graphics.PreferredBackBufferWidth = 800;
+            _graphics.PreferredBackBufferHeight = 500;
+            _graphics.ApplyChanges();
+
+            this.Window.Title = "Animals";
 
             base.Initialize();
         }
@@ -28,6 +37,9 @@ namespace Monogame_Topic_1___The_Basics_Assignment
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+            fieldTexture = Content.Load<Texture2D>("Field Background");
+            birdTexture = Content.Load<Texture2D>("bird");
         }
 
         protected override void Update(GameTime gameTime)
@@ -45,6 +57,13 @@ namespace Monogame_Topic_1___The_Basics_Assignment
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(fieldTexture, new Vector2(0, 0), Color.White);
+            _spriteBatch.Draw(birdTexture, new Vector2(10, 10), Color.White);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
